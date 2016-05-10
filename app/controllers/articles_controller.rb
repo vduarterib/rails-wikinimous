cfc90c41ce002f5ef5ad2641aa51b8546937e46d
Vasco Ribeiro <vduarterib@icloud.com>
2016-05-10 17:20:54 +0200

    articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,42 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.create(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to artilce_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to article_path
+  end
+
+  private
+
+  def artilce_params
+    params.require(:article).permit(:title, :content)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+require 'faker'
+
+10.times do
+  article = Article.new(
+    title: Faker::Name.title,
+    content: Faker::Lorem.paragraph
+  )
+  article.save
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
